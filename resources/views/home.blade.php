1f85ad99b96279c75f8fd1f7df737ef68fad085f
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-08-03 17:30:28 +0200

    add menu links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,9 +13,37 @@
 
 <body>
     <header>
-        <div class="container">
-            <h1></h1>
+        <div id="info-header">
+            <div class="container">
+                <ul>
+                    <li>
+                        <a href="#">
+                            DC POWER VISA
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            ADDITIONAL DC SITES
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
+        <nav id="header-nav" class="container">
+
+            <figure>
+                <img src="{{ asset('images/dc-logo.png') }}" alt="">
+            </figure>
+            <ul>
+                @foreach ($links as $link)
+                    <li>
+                        <a href="#">{{ $link }}</a>
+                    </li>
+                @endforeach
+
+            </ul>
+
+        </nav>
     </header>
 </body>
 
