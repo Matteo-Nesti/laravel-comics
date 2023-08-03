f58c4a1a98b204f05800f8ba4c71b350ca77e105
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-08-03 17:34:21 +0200

    add searchbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,14 +34,16 @@
             <figure>
                 <img src="{{ asset('images/dc-logo.png') }}" alt="">
             </figure>
+
             <ul>
                 @foreach ($links as $link)
                     <li>
                         <a href="#">{{ $link }}</a>
                     </li>
                 @endforeach
-
             </ul>
+
+            <input type="text" placeholder="Search">
 
         </nav>
     </header>
