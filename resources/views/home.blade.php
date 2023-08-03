1d4a2d3d32f16fccbab8149bb7485f2363cd6823
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-08-03 16:42:53 +0200

    add main set up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DC COMICS</title>
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}">
 
     @vite('resources/js/app.js')
 </head>
 
 <body>
     <header>
-
+        <div class="container">
+            <h1></h1>
+        </div>
     </header>
 </body>
 
