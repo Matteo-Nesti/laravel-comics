13df9fdb5d4b4641ac6281790f38ddf71e905c1d
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-08-21 16:50:27 +0200

    add footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -111,6 +111,161 @@
         </section>
 
     </main>
+
+    <footer>
+        <section id="top-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="links">
+
+
+                        <ul>
+                            <li>
+                                <h3>DC COMICS</h3>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                        </ul>
+
+                        <ul>
+                            <li>
+                                <h3>SHOP</h3>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                        </ul>
+
+                    </div>
+
+                    <div class="links">
+                        <ul>
+                            <li>
+                                <h3>DC</h3>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="links">
+                        <ul>
+                            <li>
+                                <h3>SITES</h3>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                            <li>
+                                <a href="#">lorem</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="logo-bg">
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+        <section id="bottom-footer">
+
+            <div class="container">
+
+                <div class="row">
+                    <div>
+                        <a href="#" class="btn-footer">SIGN UP NOW!</a>
+                    </div>
+
+                    <div class="footer-social-link">
+                        <h4>FOLLOW US</h4>
+                        <a href="#">
+                            <img src="{{ asset('images/footer-facebook.png') }}" alt="">
+                        </a>
+
+                        <a href="#">
+                            <img src="{{ asset('images/footer-periscope.png') }}" alt="">
+                        </a>
+
+                        <a href="#">
+                            <img src="{{ asset('images/footer-twitter.png') }}" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="{{ asset('images/footer-youtube.png') }}" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="{{ asset('images/footer-pinterest.png') }}" alt="">
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+    </footer>
 </body>
 
 </html>
